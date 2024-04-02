ddee2b8d5c9ed51b4f2fb47e2db13ad640634253
Emil <emil.walfridsson@icloud.com>
2024-04-02 18:05:58 +0200

    hej

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import express from "express";
+import cors from "cors"
+import jwt from "jsonwebtoken"
 
 const prisma = new PrismaClient();
