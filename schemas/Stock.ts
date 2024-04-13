3e463ba076c3266b078dda93e63663d2d5b40a22
Adam Ismail <adam_ismaail98@outlook.com>
2024-04-13 21:12:53 +0200

    Changed prisma stock model and stock schema

--- schemas/Stock.ts
+++ schemas/Stock.ts
@@ -1,20 +1,7 @@
 import { z } from "zod";
 
 const schema = z.object({
-  name: z.string().min(1, { message: "Name is required" }),
-  symbol: z.string().min(1, { message: "symbol is required" }),
-  date: z.string().min(1, { message: "date is required" }),
-  high: z
-    .number()
-    .gte(1, { message: "High must be greater than or equal to 1" }),
-  low: z.number().gte(1, { message: "Low must be greater than or equal to 1" }),
-  close: z
-    .number()
-    .gte(1, { message: "Close must be greater than or equal to 1" }),
-  volume: z
-    .number()
-    .gte(1, { message: "Volume must be greater than or equal to 1" }),
-  isFavored: z.boolean().optional(),
+  ticker: z.string().min(1, { message: "ticker is required" }),
 });
 
 export type FoodData = z.infer<typeof schema>;
