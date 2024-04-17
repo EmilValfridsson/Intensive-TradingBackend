0084b4becd3351be4da2d21178313c7f95152a8e
Adam Ismail <adam_ismaail98@outlook.com>
2024-04-17 19:26:33 +0200

    Added favoritestock router and post route to add ticker

---
+++ routes/favoriteStocks.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { validate } from "../schemas/Stock";
+import { PrismaClient } from "@prisma/client";
+import auth from "../middleware/auth";
+
+const router = express.Router();
+const prisma = new PrismaClient();
+
+router.post("/", auth, async (req, res) => {
+  const validation = validate(req.body);
+
+  if (!validation.success)
+    return res.status(400).send(validation.error.issues[0].message);
+
+  const activeUser = await prisma.user.findFirst({
+    where: { id: req.userId },
+  });
+
+  if (!activeUser) return res.status(404).send("user not found");
+
+  const ticker = await prisma.favoriteStock.create({
+    data: {
+      ticker: req.body.ticker,
+      userId: activeUser.id,
+    },
+  });
+
+  return res.status(201).send(ticker);
+});
+
+export default router;
